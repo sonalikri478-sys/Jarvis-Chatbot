210c5b1c27a03f8db51b36e3d373e4fd4390d86e
Sonali Kumari <sonalikri478@gmail.com>
2026-06-23 21:27:27 +0530

    Initial Jarvis Django Project

---
+++ Chatbot/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class ChatHistory(models.Model):
+    user_message = models.TextField()
+    jarvis_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
